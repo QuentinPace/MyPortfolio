4f15d6abe369add036eaa9227a6b462ebc8f060d
QuentinPace <quentinepace@gmail.com>
2024-12-13 10:55:38 -0500

    added to router

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,9 +1,14 @@
 import { createBrowserRouter } from 'react-router-dom';
 import Home from "../components/Home/Home.tsx"
+import TestComp from '../components/TestComp/TestComp.tsx';
 
 export const router = createBrowserRouter([
     {
         path: "/",
         element: <Home />
+    },
+    {
+        path: "/test",
+        element: <TestComp />
     }
 ])
