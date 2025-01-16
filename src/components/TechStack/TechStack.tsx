f2913c3491c68f1470009057acc1538c67d18406
QuentinPace <quentinepace@gmail.com>
2025-01-16 11:41:33 -0500

    reacting icons

--- src/components/TechStack/TechStack.tsx
+++ src/components/TechStack/TechStack.tsx
@@ -1,9 +1,28 @@
 import "./TechStack.css"
+import PostmanIcon from "../PostmanIcon"
+import NodeJsIcon from "../NodeJsIcon"
+import PostgreSQLIcon from "../PostgreSQLIcon"
+import AwsIcon from "../AWSIcon"
+import JavascriptIcon from "../JavascriptIcon"
+import ReactIcon from "../ReactIcon"
+import ReduxIcon from "../ReduxIcon"
+import TypescriptIcon from "../TypescriptIcon"
+import ViteIcon from "../ViteIcon"
+import PythonIcon from "../PythonIcon"
 
 export default function TechStack () {
     return (
         <section className="tech-stack-container">
-
+          <PostmanIcon />
+          <NodeJsIcon />
+          <PostgreSQLIcon />
+          <AwsIcon />
+          <JavascriptIcon />
+          <ReactIcon />
+          <ReduxIcon />
+          <TypescriptIcon />
+          <ViteIcon />
+          <PythonIcon />
         </section>
     ) 
 }
