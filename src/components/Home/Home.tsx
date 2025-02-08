6c6cd5ca607b3f61ef59ee318e8c705a88e29788
QuentinPace <quentinepace@gmail.com>
2025-02-08 14:33:50 -0500

    added hover arrow on get resume

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -1,50 +1,52 @@
-import portfolioObj from "../../portfolioObj"
-import "./Home.scss"
+import portfolioObj from "../../portfolioObj";
+import "./Home.scss";
 import SlideInDiv from "../SlideInDiv/SlideInDiv";
 import { Link } from "react-scroll";
 import { motion } from "framer-motion";
 
-export default function Home () {
+export default function Home() {
+  const handleGetResumeClick = () => {
+    window.open(portfolioObj.resumeDocUrl, "_blank", "noopener,noreferrer");
+  };
 
-    const handleGetResumeClick = () => {
-        window.open(portfolioObj.resumeDocUrl, "_blank", "noopener,noreferrer");
-      };
-
-    return (
-        <main className="main-home-container">
-            <section className="main-home">
-                <SlideInDiv fromLeft={true} className="home-left">
-                    <div className="home-text-container">
-                        <h1>{portfolioObj.name}</h1>
-                        <h2>{portfolioObj.jobTitle}</h2>
-                    </div>
-                    <button className= "get-resume" onClick= {handleGetResumeClick}>Get my resume</button>
-                </SlideInDiv>
-                <div className="home-right">
-                    {/* <SlideInDiv fromLeft={false} className="main-headshot" style={{"backgroundImage": `url(${portfolioObj.headshotPicUrl})`}}></SlideInDiv> */}
-                    <motion.div
-                        className="main-headshot"
-                        style={{"backgroundImage": `url(${portfolioObj.headshotPicUrl})`}}
-                        initial={{ opacity: 0, scale: 0 }}
-                        animate={{ opacity: 1, scale: 1 }}
-                        transition={{
-                            duration: 0.4,
-                            scale: { type: "spring", visualDuration: 0.4, bounce: 0.5 },
-                        }}
-                    />
-                </div>
-            </section>
-            <div className="scroll-down-container">
-                <Link
-                to="section2"
-                smooth={true}
-                duration={500}
-                className="scroll-animation-container">
-                    <div className="chevron"></div>
-                    <div className="chevron"></div>
-                    <div className="chevron"></div>
-                </Link>
-            </div>
-        </main>
-    )
+  return (
+    <main className="main-home-container">
+      <section className="main-home">
+        <SlideInDiv fromLeft={true} className="home-left">
+          <div className="home-text-container">
+            <h1>{portfolioObj.name}</h1>
+            <h2>{portfolioObj.jobTitle}</h2>
+          </div>
+          <button className="get-resume" onClick={handleGetResumeClick}>
+            Get my resume <span className="arrow">→</span>
+          </button>
+        </SlideInDiv>
+        <div className="home-right">
+          {/* <SlideInDiv fromLeft={false} className="main-headshot" style={{"backgroundImage": `url(${portfolioObj.headshotPicUrl})`}}></SlideInDiv> */}
+          <motion.div
+            className="main-headshot"
+            style={{ backgroundImage: `url(${portfolioObj.headshotPicUrl})` }}
+            initial={{ opacity: 0, scale: 0 }}
+            animate={{ opacity: 1, scale: 1 }}
+            transition={{
+              duration: 0.4,
+              scale: { type: "spring", visualDuration: 0.4, bounce: 0.5 },
+            }}
+          />
+        </div>
+      </section>
+      <div className="scroll-down-container">
+        <Link
+          to="section2"
+          smooth={true}
+          duration={500}
+          className="scroll-animation-container"
+        >
+          <div className="chevron"></div>
+          <div className="chevron"></div>
+          <div className="chevron"></div>
+        </Link>
+      </div>
+    </main>
+  );
 }
